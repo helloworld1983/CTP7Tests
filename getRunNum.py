5acb4a77e020a981235c71c3a0c89f4d38756014
Laura Dodd <ldodd@cern.ch>
2015-02-10 17:32:37 +0100

    upfating getRunNum

--- getRunNum.py
+++ getRunNum.py
@@ -4,6 +4,7 @@ x=raw_input()
 print 'cern_password',
 y=raw_input()
 cmd="wget --no-check-certificate --user="+x+" --password="+y+" https://cmsdaq0.cern.ch/cmswbm/cmsdb/runSummary/RunSummary.html"
+os.system("rm RunSummary.html")
 os.system(cmd)
 os.system("root -l -q lumi_RunSummary.C")
 
