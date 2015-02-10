e54835f89488ff56e68579b17116f40c0b197af8
Laura Dodd <ldodd@cern.ch>
2015-02-10 17:25:55 +0100

    Adding password submission run num

---
+++ getRunNum.py
@@ -0,0 +1,9 @@
+import os
+print 'cern username',
+x=raw_input()
+print 'cern_password',
+y=raw_input()
+cmd="wget --no-check-certificate --user="+x+" --password="+y+" https://cmsdaq0.cern.ch/cmswbm/cmsdb/runSummary/RunSummary.html"
+os.system(cmd)
+os.system("root -l -q lumi_RunSummary.C")
+
