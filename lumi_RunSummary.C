8e85ddf2d756b746d5aa78aa04cf66619da3c4a5
Laura Dodd <ldodd@cern.ch>
2015-02-10 17:44:36 +0100

    Removing couts in lumi html

--- lumi_RunSummary.C
+++ lumi_RunSummary.C
@@ -8,9 +8,6 @@ float lumi_RunSummary()
 	std::string str1 ("?RUN=");
 	std::string str2 ("RunSummary.html");
 	ifstream inFile("RunSummary.html");
-	//std::string str1 ("runNumber");
-	//std::string str2 ("PageZero.xml");
-	//ifstream inFile("PageZero.xml");
 	if (!inFile) {
 		cerr << "File RunSummary.html not found." << endl;
 		return -1;
@@ -19,18 +16,14 @@ float lumi_RunSummary()
 	string line;
 	while (getline(inFile, line)) {
 		if (line.empty()) continue;
-		std::cout << "Finding: " << str1 <<" In: "<<str2<<'\n';
 		std::size_t found = line.rfind(str1);
 		if (found!=std::string::npos){
 			unsigned found_begin = line.find("RUN=");
 			std::string str_a=line.substr(found_begin+4);
-			std::cout<<"String A: "<<str_a<<std::endl;
 			unsigned found_end = str_a.find("&DB");
 			std::string value_str = str_a.substr(0,found_end);
-			std::cout<<"String value: "<<value_str<<std::endl;
 			const char *cstr = value_str.c_str();
 			float value =atof(cstr);
-			std::cout<<"Float value: "<<value<<std::endl;
 			return value;
 		}
 	}
