1ba6c88b82b762d7725f998e78944a1286f94963
Laura  Dodd <ldodd@cern.ch>
2015-02-10 18:43:44 +0100

    Update getRunNum.py

--- getRunNum.py
+++ getRunNum.py
@@ -1,7 +1,7 @@
 import os
-print 'cern username',
+print 'CERN username: ',
 x=raw_input()
-print 'cern_password',
+print 'CERN password: ',
 y=raw_input()
 cmd="wget --no-check-certificate --user="+x+" --password="+y+" https://cmsdaq0.cern.ch/cmswbm/cmsdb/runSummary/RunSummary.html"
 os.system("rm RunSummary.html")
